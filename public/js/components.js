b9ed204f79739d51958e8626b9242dd748005467
Matthew Herman <mh3908@nyu.edu>
2018-12-01 11:33:56 -0500

    created divs and structure for display poll page

--- public/js/components.js
+++ public/js/components.js
@@ -1,7 +1,10 @@
 // Assumptions:
 // I assume in a complex app there would be a frontend system for displaying errors, which I do not implement here, though I do keep an error state on the Vue objects "error" property
+
+// Today's poll has id === 1, I assume this info could be gotten in an API request
 Vue.component('poll', {
   props: {
+    id: Number,
     poll: String,
     title: String,
     date: Number,
@@ -23,10 +26,10 @@ Vue.component('poll', {
     }
   },
   template: `
-  <div class="poll">
+  <div v-if="id == 1" class="todayPoll">
     <h3>Today's Poll</h3>
-    <div class="pollContent">
-      <div class="pollContentText">
+    <div class="todayPollContent">
+      <div class="todayPollContentText">
         <h3>{{title}} {{dateString}}</h3>
         <div class="buttonContainer">
           <div class="button"
@@ -36,13 +39,22 @@ Vue.component('poll', {
           </div>
         </div>
       </div>
-      <div class="pollContentGraph">
+      <div class="todayPollContentGraph">
         <p>content</p>
       </div>
     </div>
     <p class="totalVotes">Total number of votes recorded: {{totalvotes}} </p>
-
-  </div>`
+      <hr class="pollhr">
+  </div>
+  <div v-else class="regPoll">
+    <div class="regPollGraph">
+    </div>
+    <div class="regPollText">
+      <h5>{{dateString}}</h5>
+      <h5>{{title}} </h5>
+    </div>
+  </div>
+  `
 });
 
 const pollContainer = new Vue({
@@ -86,7 +98,9 @@ const pollContainer = new Vue({
   <div class="pollContainer">
     <poll
       v-for="poll in polls"
+      v-if="poll.id == 1"
       v-bind:key="poll.id"
+      v-bind:id="poll.id"
       v-bind:title="poll.title"
       v-bind:date="poll.publishedDate"
       v-bind:type="poll.answer.type"
@@ -95,6 +109,21 @@ const pollContainer = new Vue({
       v-on:vote="vote"
     >
     </poll>
+    <div class="regPollContainer">
+      <poll
+        v-for="poll in polls"
+        v-if="poll.id > 1"
+        v-bind:key="poll.id"
+        v-bind:id="poll.id"
+        v-bind:title="poll.title"
+        v-bind:date="poll.publishedDate"
+        v-bind:type="poll.answer.type"
+        v-bind:options="poll.answer.options"
+        v-bind:months="months"
+        v-on:vote="vote"
+      >
+      </poll>
+    </div>
   </div>`
 });
 
